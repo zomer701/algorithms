cfab55fe8610dbc1230836734a7d045f207a0bda
Mykhailo Markhain <zambezipro@gmail.com>
2023-02-17 05:32:35 +0200

    2131 add

--- src/aws/Leetcode_2131.java
+++ src/aws/Leetcode_2131.java
@@ -3,27 +3,29 @@ package aws;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class Leetcode_2131 {
     public static void main(String[] args) {
-        String[] words = {"lc", "cl", "gg"};
+        String[] words = {"bb","bb"};
+
         System.out.println(longestPalindrome(words));
     }
 
     public static int longestPalindrome(String[] words) {
-        HashMap<String, Integer> count = new HashMap<String, Integer>();
+        HashMap<String, Integer> count = new HashMap<>();
         for (String word : words) {
-            Integer countOfTheWord = count.get(word);
-            if (countOfTheWord == null) {
-                count.put(word, 1);
-            } else {
-                count.put(word, countOfTheWord + 1);
-            }
+            count.merge(word, 1, Integer::sum);
         }
         int answer = 0;
         boolean central = false;
+        Set<String> skip = new HashSet<>();
         for (Map.Entry<String, Integer> entry : count.entrySet()) {
+            if (skip.contains(entry.getKey())) {
+                continue;
+            }
             String word = entry.getKey();
             int countOfTheWord = entry.getValue();
             // if the word is a palindrome
@@ -34,13 +36,17 @@ public class Leetcode_2131 {
                     answer += countOfTheWord - 1;
                     central = true;
                 }
+                continue;
                 // consider a pair of non-palindrome words such that one is the reverse of another
-            } else if (word.charAt(0) < word.charAt(1)) {
-                String reversedWord = "" + word.charAt(1) + word.charAt(0);
-                if (count.containsKey(reversedWord)) {
-                    answer += 2 * Math.min(countOfTheWord, count.get(reversedWord));
-                }
             }
+
+            String reversedWord = "" + word.charAt(1) + word.charAt(0);
+            if (count.containsKey(reversedWord)) {
+                answer += 2 * Math.min(countOfTheWord, count.get(reversedWord));
+                skip.add(reversedWord);
+            }
+
+
         }
         if (central) {
             answer++;
