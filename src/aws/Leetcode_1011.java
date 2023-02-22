5057dca832af069d9fb4d7a6d28b7f9f16774280
Mykhailo Markhain <zambezipro@gmail.com>
2023-02-22 05:34:39 +0200

    1011

---
+++ src/aws/Leetcode_1011.java
@@ -0,0 +1,44 @@
+package aws;
+
+import java.util.Arrays;
+
+public class Leetcode_1011 {
+    public static void main(String[] args) {
+        int[] weights = {1,2,3,4,5,6,7,8,9,10};
+        int days = 5;
+        System.out.println(shipWithinDays(weights, days));
+    }
+
+    static Boolean feasible(int[] weights, int c, int days) {
+        int daysNeeded = 1, currentLoad = 0;
+        for (int weight : weights) {
+            currentLoad += weight;
+            if (currentLoad > c) {
+                daysNeeded++;
+                currentLoad = weight;
+            }
+        }
+
+        return daysNeeded <= days;
+    }
+
+    public static int shipWithinDays(int[] weights, int days) {
+        int totalLoad = 0, maxLoad = 0;
+        for (int weight : weights) {
+            totalLoad += weight;
+            maxLoad = Math.max(maxLoad, weight);
+        }
+
+        int l = maxLoad, r = totalLoad;
+
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (feasible(weights, mid, days)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}
