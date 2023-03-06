dbd4ef4f2576b43b2e1338d25fdc70938054e739
Mykhailo Markhain <zambezipro@gmail.com>
2023-03-06 05:46:12 +0200

    90/136/1087

--- src/aws/Leetcode_136.java
+++ src/aws/Leetcode_136.java
@@ -14,14 +14,14 @@ public class Leetcode_136 {
         }
 
         Arrays.sort(nums);
-        for (int i = 1; i < nums.length; i++) {
-            if (i % 2 != 0 && nums[i-1] != nums[i]) {
-                return nums[i-1];
-            }
-
-            if (i == nums.length -1 && i % 2 == 0) {
+        for (int i = 0; i < nums.length; i+=2) {
+            if (i+1 < nums.length && nums[i] != nums[i+1]) {
                 return nums[i];
             }
+        }
+
+        if (nums.length % 2 != 0) {
+            return nums[nums.length-1];
         }
 
         return -1;
