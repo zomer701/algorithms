7ab5981cb9e6e493ce99c015c3bb09e3c65430bb
Mykhailo Markhain <mmarkhain@totalwine.com>
2024-05-02 21:55:06 -0400

    203/338/929

--- src/trello/Leetcode_203.java
+++ src/trello/Leetcode_203.java
@@ -1,68 +1,29 @@
 package trello;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import structures.ListNode;
 
 public class Leetcode_203 {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,1,2,3};
-        int k = 2;
-        System.out.println(containsNearbyDuplicate(nums, k));
+
     }
 
-    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+    public static ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
+        ListNode currentHead = head;
 
-        if (nums == null || nums.length <= 1) {
-            return false;
-
-        }
-
-
-        Map<Integer, List<Integer>> memo = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            List<Integer> data = memo.getOrDefault(nums[i], new ArrayList<Integer>());
-            data.add(i);
-            memo.put(nums[i], data);
-        }
-
-        for (Integer key: memo.keySet()) {
-            if (memo.get(key).size() > 1) {
-                for (int i = 0; i < memo.get(key).size() -1 ; i++) {
-                    if (Math.abs(memo.get(key).get(i) - memo.get(key).get(i+1)) <= k) {
-                        return true;
-                    }
-                }
-
+        while (currentHead != null) {
+            int current = (int)currentHead.data;
+            if (current != val) {
+                prev.next = currentHead.next;
+            } else {
+                prev = prev.next;
             }
-        }
-
-
-        return false;
-    }
-
-    public boolean containsNearbyDuplicate3(int[] nums, int k) {
-
-        if (nums == null || nums.length <= 1) {
-            return false;
+            currentHead = currentHead.next;
 
         }
 
 
-        Map<Integer, Integer> memo = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            if(memo.containsKey(nums[i])){
-                int temp = memo.get(nums[i]);
-                if(Math.abs(temp - i) <= k){
-                    return true;
-                }
-            }
-            memo.put(nums[i], i);
-        }
-
-
-
-        return false;
+        return dummy.next;
     }
 }
